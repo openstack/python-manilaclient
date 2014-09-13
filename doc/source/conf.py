9b99c92c6d8220d111d57bdec2ce057868eb77c9
Andreas Jaeger <aj@suse.de>
2014-09-13 09:54:01 +0200

    Stop using intersphinx
    Remove intersphinx from the docs build as it triggers network calls that
    occasionally fail, and we don't really use intersphinx (links other
    sphinx documents out on the internet)
    
    This also removes the requirement for internet access during docs build.
    
    This can cause docs jobs to fail if the project errors out on
    warnings.
    
    Change-Id: I71e941e2a639641a662a163c682eb86d51de42fb
    Closes-Bug: #1368910

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, ROOT)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -199,7 +199,3 @@ latex_documents = [
 
 # If false, no module index is generated.
 #latex_use_modindex = True
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
