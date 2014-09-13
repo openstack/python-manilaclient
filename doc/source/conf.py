af8a53d0874d8ca02f29fc51a230971d1e31766b
Andreas Jaeger <aj@suse.de>
2014-09-13 15:07:23 +0200

    Use oslosphinx
    Since manila is now an incubated project, use oslosphinx together with
    the incubated theme.
    Unset html_static_path since there's currently no static file and sphinx
    warns otherwise.
    
    Change-Id: Icfbf2c0ee214da870c3c711a34b6a649e9878659

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, ROOT)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'oslosphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -97,12 +97,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'nature'
+#html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'incubating': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -126,7 +126,7 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
