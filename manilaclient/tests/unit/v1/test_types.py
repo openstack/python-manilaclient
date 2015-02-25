c2fa7205d8a0d581364d89494e7eebf6afa28d5e
Igor Malinovskiy <imalinovskiy@mirantis.com>
2015-02-25 17:36:00 +0200

    Remove redundant call to API in ShareType.get_keys() method
    In most cases ShareType resource already has all needed data,
    that's why get_keys() do redundant calls to API.
    
    Change-Id: I280c8919c29a19767d8bc0b5b9c1f217628b7357

--- manilaclient/tests/unit/v1/test_types.py
+++ manilaclient/tests/unit/v1/test_types.py
@@ -11,6 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import ddt
+import mock
+
 from manilaclient.tests.unit import utils
 from manilaclient.tests.unit.v1 import fakes
 from manilaclient.v1 import share_types
@@ -18,6 +21,7 @@ from manilaclient.v1 import share_types
 cs = fakes.FakeClient()
 
 
+@ddt.ddt
 class TypesTest(utils.TestCase):
     def test_list_types(self):
         tl = cs.share_types.list()
@@ -45,3 +49,42 @@ class TypesTest(utils.TestCase):
     def test_delete(self):
         cs.share_types.delete(1)
         cs.assert_called('DELETE', '/types/1')
+
+    def test_get_keys_from_resource_data(self):
+        manager = mock.Mock()
+        manager.api.client.get = mock.Mock(return_value=(200, {}))
+        valid_extra_specs = {'test': 'test'}
+        share_type = share_types.ShareType(mock.Mock(),
+                                           {'extra_specs': valid_extra_specs,
+                                            'name': 'test'},
+                                           loaded=True)
+
+        actual_result = share_type.get_keys()
+
+        self.assertEqual(actual_result, valid_extra_specs)
+        self.assertEqual(manager.api.client.get.call_count, 0)
+
+    @ddt.data({'prefer_resource_data': True,
+               'resource_extra_specs': {}},
+              {'prefer_resource_data': False,
+               'resource_extra_specs': {'fake': 'fake'}},
+              {'prefer_resource_data': False,
+              'resource_extra_specs': {}})
+    @ddt.unpack
+    def test_get_keys_from_api(self, prefer_resource_data,
+                               resource_extra_specs):
+        manager = mock.Mock()
+        valid_extra_specs = {'test': 'test'}
+        manager.api.client.get = mock.Mock(
+            return_value=(200, {'extra_specs': valid_extra_specs}))
+        info = {
+            'name': 'test',
+            'uuid': 'fake',
+            'extra_specs': resource_extra_specs
+        }
+        share_type = share_types.ShareType(manager, info, loaded=True)
+
+        actual_result = share_type.get_keys(prefer_resource_data)
+
+        self.assertEqual(actual_result, valid_extra_specs)
+        self.assertEqual(manager.api.client.get.call_count, 1)
