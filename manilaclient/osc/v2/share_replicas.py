31e231b7e560ec98a6d1a23e05c46ac5c204db99
archanaserver <ak31960466@gmail.com>
2021-12-01 20:59:02 +0000

    Add export location information to share replica show command
    This commit adds export location to the share replica show
    command response output.
    
    Closes-Bug: #1940701
    Change-Id: I972023f142ff2db8ff7575677403baaa5695e1ba

--- manilaclient/osc/v2/share_replicas.py
+++ manilaclient/osc/v2/share_replicas.py
@@ -16,6 +16,7 @@ from osc_lib import exceptions
 from osc_lib import utils as osc_utils
 
 from manilaclient.common._i18n import _
+from manilaclient.common import cliutils
 from manilaclient.osc import utils
 
 LOG = logging.getLogger(__name__)
@@ -183,16 +184,33 @@ class ShowShareReplica(command.ShowOne):
         parser.add_argument(
             "replica",
             metavar="<replica>",
-            help=_("ID of the share replica.")
+            help=_("ID of the share replica. Available only for "
+                   "microversion >= 2.47. ")
         )
         return parser
 
     def take_action(self, parsed_args):
         share_client = self.app.client_manager.share
 
-        replica = osc_utils.find_resource(
-            share_client.share_replicas,
+        replica = share_client.share_replicas.get(
             parsed_args.replica)
+
+        replica_export_locations = (
+            share_client.share_replica_export_locations.list(
+                share_replica=replica))
+
+        replica._info['export_locations'] = []
+        for element_location in replica_export_locations:
+            element_location._info.pop('links', None)
+            replica._info['export_locations'].append(
+                element_location._info)
+
+        if parsed_args.formatter == 'table':
+            replica._info['export_locations'] = (
+                cliutils.convert_dict_list_to_string(
+                    replica._info['export_locations']))
+
+        replica._info.pop('links', None)
 
         return self.dict2columns(replica._info)
 
