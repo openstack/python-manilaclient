6283c0f2d03e8c7e42f963b3e341323af65a681b
Maurice Escher <maurice.escher@sap.com>
2022-03-25 13:46:27 +0100

    fix manager _list() method when called from another manager
    only occurence of this was listing share_instances at shares
    
    Closes-Bug: #1966441
    Change-Id: If65dd1e256a9ee50dcda98a9a0804d4986efa014
    (cherry picked from commit eeece589207c7cf11384c3a94f84ec389b385cbe)
    (cherry picked from commit 9a33475447d81ef8e9cee7d4b6fdf72def6416e9)
    (cherry picked from commit 2c4a8a806ce58731d8eb3f29a0f204d25a25e9f5)

--- manilaclient/v2/shares.py
+++ manilaclient/v2/shares.py
@@ -729,5 +729,5 @@ class ShareManager(base.ManagerWithFind):
         return self._list(
             '/shares/%s/instances' % common_base.getid(share),
             'share_instances',
-            obj_class=share_instances.ShareInstance
+            manager=share_instances.ShareInstanceManager(self)
         )
