5c5220bbee47b443ac76d0bed4bacb33c6d0dce5
Franca Mgbogu <franciedigital@gmail.com>
2022-08-15 12:21:58 +0000

    [OSC] Implement Share Server Migration Cancel and Complete Command
    This commit adds 'openstack share server migration cancel' and
    'openstack share server migration complete' command, that implement the
    same functionality as 'manila share-server-migration-cancel'and 'manila
    share-server-migration-complete' command.
    
    Partially-implements: bp openstack-client-support
    Change-Id: Ic6e721abdc54854f1117b39ad674e7bc747adf98

--- manilaclient/osc/v2/share_servers.py
+++ manilaclient/osc/v2/share_servers.py
@@ -409,3 +409,62 @@ class SetShareServer(command.Command):
                 {'status': parsed_args.status, 'exception': e})
             LOG.error(msg)
             raise exceptions.CommandError(msg)
+
+
+class ShareServerMigrationCancel(command.Command):
+    """Attempts to cancel migration for a given share server
+
+    :param share_server: either share_server object or text with its ID.
+    """
+
+    _description = _("Cancels migration of a given share server when copying")
+
+    def get_parser(self, prog_name):
+        parser = super(ShareServerMigrationCancel, self).get_parser(prog_name)
+        parser.add_argument(
+            'share_server',
+            metavar='<share_server>',
+            help=_('ID of share server to cancel migration.')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        share_client = self.app.client_manager.share
+        share_server = osc_utils.find_resource(
+            share_client.share_servers,
+            parsed_args.share_server)
+        if share_client.api_version >= api_versions.APIVersion("2.57"):
+            share_server.migration_cancel()
+        else:
+            raise exceptions.CommandError(
+                "Share Server Migration cancel is only available "
+                "with manila API version >= 2.57")
+
+
+class ShareServerMigrationComplete(command.Command):
+    """Completes migration for a given share server (Admin only, Experimental).
+
+    """
+    _description = _("Completes migration for a given share server")
+
+    def get_parser(self, prog_name):
+        parser = super(ShareServerMigrationComplete, self).get_parser(
+            prog_name)
+        parser.add_argument(
+            'share_server',
+            metavar='<share_server>',
+            help=_('ID of share server to complete migration.')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        share_client = self.app.client_manager.share
+        share_server = osc_utils.find_resource(
+            share_client.share_servers,
+            parsed_args.share_server)
+        if share_client.api_version >= api_versions.APIVersion("2.57"):
+            share_server.migration_complete()
+        else:
+            raise exceptions.CommandError(
+                "Share Server Migration complete is only available "
+                "with manila API version >= 2.57")
